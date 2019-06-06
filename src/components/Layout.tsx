0049bb40529319f1658ad90d9ad5ee1e3ed2a543
Nathan L Smith <nsmith@newrelic.com>
2019-06-06 09:59:48 -0500

    Add head scripts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -37,6 +37,26 @@ export const Layout: FunctionComponent<LayoutProps> = ({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
         />
+        <script src="https://global.localizecdn.com/localize.js" />
+        <script>{`
+          !function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
+
+          Localize.initialize({
+            key: 'localize-key',
+            rememberLanguage: true,
+            saveNewPhrasesFromSource: true
+          });
+
+          // Hotjar Tracking Code for https://adoptopenjdk.net
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:908427,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}</script>
       </Helmet>
       <Header />
       <main className={`grey-bg ${className}`}>{children}</main>
