d547b6366089a5f8f59278c9029f1a365784b941
bswitzer <bswitzer@newrelic.com>
2019-06-14 10:12:53 -0700

    task(content): Adds markup from handlebars

--- src/pages/nightly.tsx
+++ src/pages/nightly.tsx
@@ -1,16 +1,115 @@
-import React, { FunctionComponent, SyntheticEvent } from "react"
+import React, {FunctionComponent, SyntheticEvent} from "react"
 import Layout from "../components/Layout"
 import ReleaseSelector from "../components/ReleaseSelector";
 
-export interface NightlyProps {}
+export interface NightlyProps {
+}
 
 const Nightly: FunctionComponent<NightlyProps> = props => {
+
+    const thisInstallerLink = false;
+    const platforms = [
+        {
+            placeholder: true
+        }
+    ]
+
     return (
         <Layout>
+            <h1 className="large-title">Nightly builds</h1>
+            <a href="./releases" className="blue-button a-button">
+                <div>
+                    <span>Latest Release</span>
+                    <i className="fa fa-arrow-circle-o-right" aria-hidden="true"/>
+                </div>
+            </a>
+            <a href="./archive" id="nightly-button" className="grey-button a-button">
+                <div>
+                    <span>Release Archive</span>
+                    <i className="fa fa-arrow-circle-o-right" aria-hidden="true"/>
+                </div>
+            </a>
             <ReleaseSelector
-                onVersionChange={(e: SyntheticEvent)=> {console.log(e)}}
-                onJVMChange={(e: SyntheticEvent)=> {console.log(e)}}
+                onVersionChange={(e: SyntheticEvent) => {
+                    console.log(e)
+                }}
+                onJVMChange={(e: SyntheticEvent) => {
+                    console.log(e)
+                }}
             />
+
+            <div>
+                <span>View</span>
+                <select id="numberpicker">
+                    <option value="10">10</option>
+                    <option value="20" selected={true}>20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+                <span>builds prior to: </span>
+                <input type="text" id="datepicker"/>
+            </div>
+
+            <input type="text" className="block margin-auto" id="search" placeholder="Search these builds"/>
+
+            <h3 id="scroll-text" className="hide">Scroll horizontally <i className="fa fa-arrow-circle-o-right"
+                                                                         aria-hidden="true"/></h3>
+
+            <div id="search-error" className="hide">No search results</div>
+
+            <div id="loading"><img src="dist/assets/loading_dots.gif" width="40" height="40" alt="Content is loading."/>
+            </div>
+
+            <div id="nightly-list">
+                <table id="table-parent">
+                    <thead id="table-head">
+                    <tr id='table-header'>
+                        <th>Platform</th>
+                        <th>Type</th>
+                        <th>Date</th>
+                        <th>Binary</th>
+                        <th>Installer</th>
+                        <th>Checksum</th>
+                    </tr>
+                    </thead>
+                    <tbody id="nightly-table" className="hide">
+                    { platforms.map(() => {
+                        return (
+                            <tr className='nightly-container'>
+                                <td className='nightly-platform-block'>Linux aarch64</td>
+                                <td className='nightly-platform-type'>jdk</td>
+                                <td>
+                                    <div className='nightly-release-date'>13 June 2019</div>
+                                </td>
+                                <td className='nightly-downloads-block'>
+                                    <div>
+                                        <a className='dark-link' href="">
+                                            .tar.gz (99 MB)
+                                        </a>
+                                    </div>
+                                </td>
+                                {
+                                    thisInstallerLink ?
+                                        <td className='nightly-downloads-block'>
+                                            <div>
+                                                <a className='dark-link' href="">
+                                                    .pkg
+                                                </a>
+                                            </div>
+                                        </td>
+                                        :
+                                        <td className='nightly-downloads-block'>
+                                            <div>Not available</div>
+                                        </td>
+                                }
+                                <td><a href="" className='dark-link'>Checksum</a></td>
+                            </tr>
+                        )
+                    })
+                    }
+                    </tbody>
+                </table>
+            </div>
         </Layout>
     )
 }
@@ -18,3 +117,4 @@ const Nightly: FunctionComponent<NightlyProps> = props => {
 export default Nightly;
 
 
+export const Night
