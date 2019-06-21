79c15c891ef0dc84abf86fcbfa106a19b1c0ef2e
Nathan L Smith <nsmith@newrelic.com>
2019-06-21 14:37:27 -0500

    Start adding home page

--- src/components/ReleaseSelector.tsx
+++ src/components/ReleaseSelector.tsx
@@ -1,54 +1,102 @@
-import React, { FunctionComponent, SyntheticEvent } from 'react'
-import {ChoiceGroup} from "office-ui-fabric-react";
+import React, { FunctionComponent, SyntheticEvent } from "react"
+
+import { ChoiceGroup } from "office-ui-fabric-react"
 
 export interface ReleaseSelectorProps {
-    onVersionChange(e: SyntheticEvent): void;
-    onJVMChange(e: SyntheticEvent): void;
+  onVersionChange(e: SyntheticEvent): void
+  onJVMChange(e: SyntheticEvent): void
 }
 
-const ReleaseSelector: FunctionComponent<ReleaseSelectorProps> = ({onVersionChange, onJVMChange}) => {
-    return (
-      <div className="btn-container">
-        <form id="jdk-selector" className="btn-form">
-          <h3>1. Choose a Version</h3>
-          <ChoiceGroup
-            defaultSelectedKey="A"
-            onChange={onVersionChange}
-            options={[
-                {
-                    key: 'A',
-                    text: 'OpenJDK 8 (LDS)'
-                },
-                {
-                    key: 'B',
-                    text: 'OpenJDK 9'
-                },
-                {
-                    key: 'C',
-                    text: 'OpenJDK 10'
-                }
-            ]}
-          />
-        </form>
-        <form id="jvm-selector" className="btn-form">
-          <h3>2. Choose a JVM</h3>
-          <ChoiceGroup
-            defaultSelectedKey="A"
-            onChange={onJVMChange}
-            options={[
-                {
-                    key: 'A',
-                    text: 'HotSpot'
-                },
-                {
-                    key: 'B',
-                    text: 'OpenJ9'
-                }
-            ]}
-          />
-        </form>
-      </div>
-    )
+const ReleaseSelector: FunctionComponent<ReleaseSelectorProps> = ({
+  onVersionChange,
+  onJVMChange,
+}) => {
+  return (
+    <div className="btn-container">
+      <form id="jdk-selector" className="btn-form">
+        <h3>1. Choose a Version</h3>
+        <ChoiceGroup
+          defaultSelectedKey="A"
+          onChange={onVersionChange}
+          options={[
+            {
+              key: "A",
+              text: "OpenJDK 8 (LDS)",
+            },
+            {
+              key: "B",
+              text: "OpenJDK 9",
+            },
+            {
+              key: "C",
+              text: "OpenJDK 10",
+            },
+          ]}
+        />
+      </form>
+      <form id="jvm-selector" className="btn-form">
+        <h3>
+          2. Choose a JVM
+          <span className="help-jvm">Help Me Choose</span>
+          <div className="popup-content" id="jvm-popup">
+            <div className="popup-heading">
+              <h4>Choosing the right JVM</h4>
+            </div>
+            <div className="popup-text">
+              <p className="hotspot-text">
+                <b>HotSpot</b> is the VM from the OpenJDK community. It is the
+                most widely used VM today and is used in Oracle’s JDK. It is
+                suitable for all workloads.
+              </p>
+              <p>
+                For more details see{" "}
+                <a
+                  rel="noopener"
+                  target="_blank"
+                  href="https://openjdk.java.net/groups/hotspot/"
+                >
+                  OpenJDK HotSpot
+                </a>
+                .
+              </p>
+              <p className="openj9-text">
+                <b>Eclipse OpenJ9</b> is the VM from the Eclipse community. It
+                is an enterprise-grade VM designed for low memory usage and fast
+                start-up and is used in IBM’s JDK. It is suitable for running
+                all workloads.
+              </p>
+              <p>
+                For more details see{" "}
+                <a
+                  rel="noopener"
+                  target="_blank"
+                  href="https://www.eclipse.org/openj9/"
+                >
+                  Eclipse OpenJ9
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </h3>
+
+        <ChoiceGroup
+          defaultSelectedKey="A"
+          onChange={onJVMChange}
+          options={[
+            {
+              key: "A",
+              text: "HotSpot",
+            },
+            {
+              key: "B",
+              text: "OpenJ9",
+            },
+          ]}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default ReleaseSelector
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,219 @@
-import { FontWeights, Link, Stack, Text } from "office-ui-fabric-react"
+import "../scss/styles-2-index.scss"
 
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
 import React from "react"
-
-const boldStyle = { root: { fontWeight: FontWeights.semibold } }
+import ReleaseSelector from "../components/ReleaseSelector"
+import loadingDots from "../assets/loading_dots.gif"
 
 const App: React.FunctionComponent = () => {
   return (
     <Layout>
-      <Stack
-        horizontalAlign="center"
-        verticalAlign="center"
-        verticalFill
-        styles={{
-          root: {
-            width: "960px",
-            margin: "0 auto",
-            textAlign: "center",
-            color: "#605e5c",
-          },
-        }}
-        className="App"
-        gap={15}
-      >
-        <Text variant="large" as="h1">
-          Hello
-        </Text>
-        <img
-          src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
-          alt="logo"
-        />
-        <Text variant="xxLarge" styles={boldStyle}>
-          Welcome to Your UI Fabric App
-        </Text>
-        <Text variant="large">
-          For a guide on how to customize this project, check out the UI Fabric
-          documentation.
-        </Text>
-        <Text variant="large" styles={boldStyle}>
-          Essential Links
-        </Text>
-        <Stack horizontal gap={15} horizontalAlign="center">
-          <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
-          <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-            Stack Overflow
+      <main className="grey-bg home-main">
+        <h1>Prebuilt OpenJDK Binaries for Free!</h1>
+        <p className="intro">
+          Java™ is the world's leading programming language and platform.
+          AdoptOpenJDK uses{" "}
+          <a
+            href="https://github.com/AdoptOpenJDK/openjdk-infrastructure"
+            rel="noopener"
+            target="_blank"
+          >
+            infrastructure
+          </a>
+          ,{" "}
+          <a
+            href="https://github.com/AdoptOpenJDK/openjdk-build"
+            rel="noopener"
+            target="_blank"
+          >
+            build
+          </a>
+          , and{" "}
+          <a
+            href="https://github.com/AdoptOpenJDK/openjdk-tests"
+            rel="noopener"
+            target="_blank"
+          >
+            test
+          </a>{" "}
+          scripts to produce prebuilt binaries from{" "}
+          <a href="https://openjdk.java.net" rel="noopener" target="_blank">
+            OpenJDK
+          </a>
+          ™ class libraries and a choice of either the{" "}
+          <a
+            rel="noopener"
+            target="_blank"
+            href="https://openjdk.java.net/groups/hotspot/"
+          >
+            OpenJDK HotSpot
+          </a>{" "}
+          or{" "}
+          <a
+            rel="noopener"
+            target="_blank"
+            href="https://www.eclipse.org/openj9/"
+          >
+            Eclipse OpenJ9
+          </a>{" "}
+          VM.
+          <br />
+          All AdoptOpenJDK binaries and scripts are{" "}
+          <Link to="/about" rel="noopener" target="_blank">
+            open source licensed
+          </Link>{" "}
+          and available for free.
+        </p>
+
+        <div className="dl-container">
+          <h2 id="dl-text" className="invisible">
+            Downloads
+          </h2>
+          <ReleaseSelector onVersionChange={_ => {}} onJVMChange={_ => {}} />
+          <div id="loading">
+            <img
+              src={loadingDots}
+              height="40"
+              width="40"
+              alt="Content is loading."
+            />
+          </div>
+          <div id="error-container" />
+          <Link
+            to="/releases"
+            className="dl-button a-button fadeIn invisible"
+            id="dl-latest"
+          >
+            <div id="dl-version">
+              <i id="dl-icon" className="fa fa-download" aria-hidden="true" />
+              <span>Latest release</span>
+              <i
+                id="dl-icon-2"
+                className="fa fa-arrow-circle-o-right hide"
+                aria-hidden="true"
+              />
+            </div>
+            <div className="dl-description">
+              <var id="dl-version-text" className="small-dl-text" />
+              <div
+                id="jck-approved-tick"
+                className="tick hide"
+                data-tooltip="This build is JCK certified."
+              >
+                <img
+                  src="./dist/assets/tick.png"
+                  alt="This build is JCK certified."
+                />
+              </div>
+            </div>
           </Link>
-          <Link href="https://github.com/officeDev/office-ui-fabric-react/">
-            Github
+          <Link
+            to="/releases"
+            className="dl-button dl-thin-button a-button fadeIn invisible"
+            id="dl-other"
+          >
+            <div>
+              <span>Other platforms</span>
+              <i className="fa fa-arrow-circle-o-right" aria-hidden="true" />
+            </div>
           </Link>
-          <Link href="https://twitter.com/officeuifabric">Twitter</Link>
-        </Stack>
-        <Text variant="large" styles={boldStyle}>
-          Design System
-        </Text>
-        <Stack horizontal gap={15} horizontalAlign="center">
-          <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">
-            Icons
+          <Link
+            to="/archive"
+            className="dl-button dl-thin-button a-button fadeIn invisible"
+            id="dl-archive"
+          >
+            <div>
+              <span>Release Archive &amp; Nightly Builds</span>
+              <i className="fa fa-archive" aria-hidden="true" />
+            </div>
           </Link>
-          <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">
-            Typography
+          AdoptOpenJDK now also distributes{" "}
+          <strong>
+            <Link style={{ textDecoration: "none" }} to="/upstream">
+              OpenJDK upstream builds
+            </Link>
+          </strong>
+          !
+        </div>
+
+        <div style={{ marginTop: "3rem" }}>
+          <Link to="/installation" className="no-underline home-links">
+            <h2 id="installation-link" className="inline-block zero-margin">
+              Installation&nbsp;
+              <i className="fa fa-arrow-circle-o-right" aria-hidden="true" />
+            </h2>
           </Link>
-          <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">
-            Theme
+          <Link to="/migration" className="no-underline home-links">
+            <h2 id="migration-link" className="inline-block zero-margin">
+              Migration&nbsp;
+              <i className="fa fa-arrow-circle-o-right" aria-hidden="true" />
+            </h2>
           </Link>
-        </Stack>
-      </Stack>
+          <Link to="/support" className="no-underline home-links">
+            <h2 id="support-link" className="inline-block zero-margin">
+              Support&nbsp;
+              <i className="fa fa-arrow-circle-o-right" aria-hidden="true" />
+            </h2>
+          </Link>
+          <Link
+            to="/getinvolved"
+            id="dev-button"
+            className="no-underline home-links"
+          >
+            <h2 className="inline-block zero-margin">
+              Get involved&nbsp;
+              <i className="fa fa-arrow-circle-o-right" aria-hidden="true" />
+            </h2>
+          </Link>
+
+          <div id="minor-links" className="blue-link-section">
+            <Link to="/about">
+              <h3 className="inline-block zero-margin">About</h3>
+            </Link>
+            <span> | </span>
+            <Link to="/sponsors">
+              <h3 className="inline-block zero-margin">Sponsors</h3>
+            </Link>
+            <span> | </span>
+            <Link to="/testimonials">
+              <h3 className="inline-block zero-margin">Testimonials</h3>
+            </Link>
+            <span> | </span>
+            <a
+              rel="noopener"
+              target="_blank"
+              href="https://api.adoptopenjdk.net"
+            >
+              <h3 className="inline-block zero-margin">API</h3>
+            </a>
+            <span> | </span>
+            <a
+              rel="noopener"
+              target="_blank"
+              href="https://blog.adoptopenjdk.net/"
+            >
+              <h3 className="inline-block zero-margin">Blog</h3>
+            </a>
+            <span> | </span>
+            <a
+              rel="noopener"
+              target="_blank"
+              href="https://calendar.adoptopenjdk.net"
+            >
+              <h3 className="inline-block zero-margin">Meeting diary</h3>
+            </a>
+            <span> | </span>
+            <a
+              rel="noopener"
+              target="_blank"
+              href="https://status.adoptopenjdk.net"
+            >
+              <h3 className="inline-block zero-margin">Status</h3>
+            </a>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
