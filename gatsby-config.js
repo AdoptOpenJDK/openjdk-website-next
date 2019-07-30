ed106c423586827061cb667bd0cd11dcafdf8cad
Nathan L Smith <nsmith@newrelic.com>
2019-07-30 17:55:51 -0500

    Start wiring up GraphQL to API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,42 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-source-apiserver`,
+      options: {
+        enableDevRefresh: true,
+        name: "assets",
+        url: "https://api.adoptopenjdk.net/v3/assets",
+        verboseOutput: true
+      }
+    },
+    {
+      resolve: `gatsby-source-apiserver`,
+      options: {
+        enableDevRefresh: true,
+        name: "avaliableReleases",
+        url: "https://api.adoptopenjdk.net/v3/info/available_releases",
+        verboseOutput: true
+      }
+    },
+    {
+      resolve: `gatsby-source-apiserver`,
+      options: {
+        enableDevRefresh: true,
+        name: "platforms",
+        url: "https://api.adoptopenjdk.net/v3/info/platforms",
+        verboseOutput: true
+      }
+    },
+    {
+      resolve: `gatsby-source-apiserver`,
+      options: {
+        enableDevRefresh: true,
+        name: "variants",
+        url: "https://api.adoptopenjdk.net/v3/info/variants",
+        verboseOutput: true
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
@@ -16,6 +52,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-client-side-redirect`,
-  ],
+    `gatsby-plugin-client-side-redirect`
+  ]
 };
