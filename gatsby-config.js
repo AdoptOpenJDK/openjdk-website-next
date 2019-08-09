915c02104f61497ee2410a30cc4cab9a56058e27
Nathan L Smith <smith@nlsmith.com>
2019-08-09 15:08:46 -0500

    Remove asset entities from api server config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,5 @@
 const apiUrl = "https://api.adoptopenjdk.net/v3";
 
-// Available releases are hard-coded here to allow for the assets endpoint
-const availableVersions = [8, 9, 10, 11, 12, 13];
-
-// Array of releases for each type
-const assets = availableVersions
-  .map(version => ["nightly", version])
-  .concat(availableVersions.map(version => ["releases", version]));
-
 const apiEntities = [
   {
     name: "avaliableReleases",
@@ -15,13 +7,8 @@ const apiEntities = [
   },
   { name: "platforms", url: `${apiUrl}/info/platforms` },
   { name: "variants", url: `${apiUrl}/info/variants` }
-].concat(
-  assets.map(([releaseType, version]) => ({
-    name: `assets${releaseType}${version}`,
-    url: `${apiUrl}/assets/${releaseType}/${version}`
-  }))
-);
-console.log(apiEntities);
+];
+
 module.exports = {
   plugins: [
     {
@@ -31,33 +18,6 @@ module.exports = {
         verboseOutput: true
       }
     },
-    // {
-    //   resolve: `gatsby-source-apiserver`,
-    //   options: {
-    //     enableDevRefresh: true,
-    //     name: "avaliableReleases",
-    //     url: `${apiUrl}/info/available_releases`,
-    //     verboseOutput: true
-    //   }
-    // },
-    // {
-    //   resolve: `gatsby-source-apiserver`,
-    //   options: {
-    //     enableDevRefresh: true,
-    //     name: "platforms",
-    //     url: `${apiUrl}/info/platforms`,
-    //     verboseOutput: true
-    //   }
-    // },
-    // {
-    //   resolve: `gatsby-source-apiserver`,
-    //   options: {
-    //     enableDevRefresh: true,
-    //     name: "variants",
-    //     url: `${apiUrl}/info/variants`,
-    //     verboseOutput: true
-    //   }
-    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
